a471820b049e2b9b7d7478ca8960b9b76d1f8251
shinoby85 <ponk@tut.by>
2021-05-31 19:20:12 +0300

    Работа с курсом Минина

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {delay} from "rxjs/operators";
 
+export interface ITodo{
+  id?: number,
+  title: string,
+  complited: boolean
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Minin';
+export class AppComponent implements OnInit{
+
+  public todos:ITodo[]=[]
+
+  todoTitle=""
+
+  public loadText=false
+
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.fetchTodos()
+  }
+
+  public addTodo():any {
+    if (!this.todoTitle.trim()){
+      return this;
+    }
+    const newTodo:ITodo={
+      title: this.todoTitle,
+      complited: false
+    }
+    this.http.post<ITodo>('https://jsonplaceholder.typicode.com/todos',newTodo)
+      .subscribe(todo=>{
+        this.todos.push(todo)
+        this.todoTitle=''
+      })
+  }
+
+  public fetchTodos() {
+    this.loadText=false
+    this.http.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .pipe(delay(3000))
+      .subscribe(todos=>{
+        this.todos=todos
+        this.loadText=true
+      })
+  }
+
+  removeTodo(id: number | undefined) {
+    this.http.delete<ITodo>(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .subscribe(()=>{
+        this.todos=this.todos.filter(todo=>todo.id!==id)
+      })
+  }
 }
