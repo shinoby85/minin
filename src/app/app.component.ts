68e2e5351c82cb550ef149e8b7132a9bc4ebd6ae
shinoby85 <ponk@tut.by>
2021-06-01 19:49:17 +0300

    Работа с курсом Минина

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,62 +1,58 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {delay} from "rxjs/operators";
+import {Component, OnInit} from '@angular/core'
+import {Todo, TodosService} from './todos.service'
 
-export interface ITodo{
-  id?: number,
-  title: string,
-  complited: boolean
-}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
-  public todos:ITodo[]=[]
+  todos: Todo[] = []
+  loading = false
+  todoTitle = ''
 
-  todoTitle=""
-
-  public loadText=false
-
-
-  constructor(private http: HttpClient) {
-  }
+  constructor(private todosService: TodosService) {}
 
   ngOnInit() {
     this.fetchTodos()
   }
 
-  public addTodo():any {
-    if (!this.todoTitle.trim()){
-      return this;
+  addTodo() {
+    if (!this.todoTitle.trim()) {
+      return
     }
-    const newTodo:ITodo={
+
+    this.todosService.addTodo({
       title: this.todoTitle,
-      complited: false
-    }
-    this.http.post<ITodo>('https://jsonplaceholder.typicode.com/todos',newTodo)
-      .subscribe(todo=>{
-        this.todos.push(todo)
-        this.todoTitle=''
-      })
+      completed: false
+    }).subscribe(todo => {
+      this.todos.push(todo)
+      this.todoTitle = ''
+    })
   }
 
-  public fetchTodos() {
-    this.loadText=false
-    this.http.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
-      .pipe(delay(3000))
-      .subscribe(todos=>{
-        this.todos=todos
-        this.loadText=true
+  fetchTodos() {
+    this.loading = true
+    this.todosService.fetchTodos()
+      .subscribe(todos => {
+        this.todos = todos
+        this.loading = false
       })
   }
 
   removeTodo(id: number | undefined) {
-    this.http.delete<ITodo>(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .subscribe(()=>{
-        this.todos=this.todos.filter(todo=>todo.id!==id)
+    this.todosService.removeTodo(id)
+      .subscribe(() => {
+        this.todos = this.todos.filter(t => t.id !== id)
       })
   }
+
+  completeTodo(id: number | undefined) {
+    this.todosService.completeTodo(id).subscribe(todo => {
+      // @ts-ignore
+      this.todos.find(t => t.id === todo.id).completed = true;
+    })
+  }
 }
+
